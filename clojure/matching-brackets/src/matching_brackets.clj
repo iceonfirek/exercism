877b83217f11aff812f788c82d8b75820013bf28
xiankun.wu <xiankun.wu@inceptio.ai>
2022-10-13 12:14:45 +0800

    update...

--- clojure/matching-brackets/src/matching_brackets.clj
+++ clojure/matching-brackets/src/matching_brackets.clj
@@ -1,19 +1,19 @@
 (ns matching-brackets)
 
-(def brac "()[]{}")
-(def brac0 "()][{}")
-(def brac1 "([{}])")
-
 (defn valid?
   ([x] (valid? x 0 0))
   ([x i j]
    (cond
      (< j 0) false
-     (and  (= i (count x)) (= 0 j)) true
+     (and (= i (count x)) (= 0 j)) true
      (and (= i (count x)) (not= 0 j)) false
      (= (nth x i) \() (valid? x (inc i) (inc j))
      (= (nth x i) \)) (valid? x (inc i) (dec j))
-     ) ))
+     (= (nth x i) \[) (valid? x (inc i) (+ 100 j))
+     (= (nth x i) \]) (valid? x (inc i) (- j 100))
+     (= (nth x i) \{) (valid? x (inc i) (+ 10000 j))
+     (= (nth x i) \}) (valid? x (inc i) (- j 10000))
+     )))
 
 
 
