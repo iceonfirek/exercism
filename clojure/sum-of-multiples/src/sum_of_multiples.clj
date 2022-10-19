205cac5fe9c480d80bfa9194d37acd8a584d731d
xiankun.wu <xiankun.wu@inceptio.ai>
2022-10-19 19:19:51 +0800

    update 40

--- clojure/sum-of-multiples/src/sum_of_multiples.clj
+++ clojure/sum-of-multiples/src/sum_of_multiples.clj
@@ -8,3 +8,17 @@
        (filter some?)
        (into #{})
        (reduce +)))
+
+;; ########################
+;; (defn sum-of-n [x n]
+;;   (cond
+;;     (every? #(= 0 (quot n %)) x) 0
+;;     (some #(= 0 (mod n %)) x) (+ n (sum-of-n x (- n 1)))
+;;     :else (sum-of-n x (- n 1))))
+
+;; (defn sum-of-multiples [x n]
+;;   (sum-of-n x (- n 1)))
+
+;; #########################
+;; (defn sum-of-1 [y n]
+;;   (reduce + (map #(quot (* % (quot (- n 1) %) (+ (quot (- n 1) %) 1)) 2) y)))
